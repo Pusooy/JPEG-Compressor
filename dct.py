9ef2887280db2e016df927fd2e7f9197272e57df
TrumpHe <73583074+TrumpHe@users.noreply.github.com>
2022-03-03 05:35:46 +0800

    Delete dct.py

--- dct.py
+++
@@ -1,8 +0,0 @@
-"""Implements the DCT."""
-import scipy.fftpack
-
-
-def dct2_scipy(two_d_array):
-    """Implements the 2d orthonormal dct using scipy (much quicker)."""
-    return scipy.fftpack.dct(scipy.fftpack.dct(two_d_array.T, norm='ortho')
-                             .T, norm='ortho')
